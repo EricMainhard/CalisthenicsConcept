f98244596c7162e3a24d14712e492fe833c7ff38
Eric Mainhard <eric.mainhard@vnstudios.com>
2022-01-26 18:10:10 -0300

    Banner content

--- src/Components/HomeBanner/HomeBanner.jsx
+++ src/Components/HomeBanner/HomeBanner.jsx
@@ -4,7 +4,17 @@ import './homebanner.css';
 export default function HomeBanner() {
   return (
     <section className='homeBanner'>
-        
+        <div className="homeBannerContent">
+          <div className="homeBannerTitle">
+            CALISTHENICS CONCEPT
+          </div>
+          <div className="homeBannerText">
+            The future of street workout
+          </div>
+          <div className="homeBannerCta">
+            SHOP
+          </div>
+        </div>
     </section>
   );
 }
--- src/Components/NavIcons/NavIcons.jsx
+++ src/Components/NavIcons/NavIcons.jsx
@@ -3,10 +3,13 @@ import './navicons.css';
 
 export default function NavIcons() {
     return (
-    <div className="navIcons">
-        <i className="navIcon fas fa-search"></i>
-        <i className="navIcon fas fa-user"></i>
-        <i className="navIcon fas fa-shopping-cart"></i>
-    </div>
+    <>  
+        <i className="navIcons menuIcon lg_hidden fas fa-ellipsis-h"></i>
+        <div className="navIcons">
+            <i className="navIcon sm_hidden fas fa-search"></i>
+            <i className="navIcon sm_hidden fas fa-user"></i>
+            <i className="navIcon sm_hidden fas fa-shopping-cart"></i>
+        </div>
+    </>
     );
 }
